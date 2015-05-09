d324d318c8a3c05a2e6699af9172dd72bf43a023
Muhammad Algshy <m.algshy@gmail.com>
2015-05-09 11:11:47 +0300

    make it better

---
+++ db/migrate/20150508121758_add_user_id_statuses.rb
@@ -0,0 +1,7 @@
+class AddUserIdStatuses < ActiveRecord::Migration
+  def change
+  	add_column :statuses, :user_id, :integer
+  	add_index :statuses, :user_id
+  	remove_column :statuses,  :name
+  end
+end
